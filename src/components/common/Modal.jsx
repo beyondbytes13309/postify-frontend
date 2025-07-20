7af77881c1bc5b82c0c4797088814067f73bb5e8
beyondbytes13309 <emmarthedev@gmail.com>
2025-07-20 11:32:43 +0500

    refactor(modal): attach function to modal that helps with modifying

--- src/components/common/Modal.jsx
+++ src/components/common/Modal.jsx
@@ -1,23 +1,35 @@
-import { useState } from 'react'
+import { useState, forwardRef, useImperativeHandle, useRef } from 'react'
 import styles from '../styles/Modal.module.css'
+
 
 import Button from './Button'
 
-export default function Modal({
-     visibility=true, 
-     setVisibility, 
-     variant="alert", 
-     buttonTexts=[], 
-     title="Default title", 
-     text="Default text", 
-     setButtonClick= ()=>{console.warn("You're seeing this probably because you didn't pass 'setButtonClick' function as a prop to the Modal component")},
-     modalInput="",
-     setModalInput=()=>{console.warn("You're seeing this probably because you didn't pass the 'setModalInput' function as a prop to the Modal component")}}) {
+export default forwardRef(function Modal({visibility=true, setVisibility}, ref) {
+
+    const [localConfig, setLocalConfig] = useState({
+        variant: 'alert',
+        buttonTexts: [],
+        title: 'Default title',
+        text: 'Default text',
+        setButtonClick: ()=>{console.warn("You're seeing this probably because you didn't pass 'setButtonClick' function as a prop to the Modal component")},
+        modalInput: "",
+        setModalInput: ()=>{console.warn("You're seeing this probably because you didn't pass the 'setModalInput' function as a prop to the Modal component")}
+    })
 
     const handleButtonClick = (btnNum) => {
-        setButtonClick(btnNum)
+        if (localConfig.setButtonClick == function(){}) {
+            localConfig.setButtonClick(btnNum)
+        }
+        
         setVisibility(false)
     }
+    
+
+    useImperativeHandle(ref, () => ({
+        modifyModal: (newConfig) => {
+            setLocalConfig((prev) => ({ ...prev, ...newConfig }))
+        }
+    }))
 
     const variants = {
         alert: "alertModal",
@@ -33,26 +45,26 @@ export default function Modal({
             
               
             {visibility && <div className={styles.modalOverlay}>
-                <div className={`${styles.wrapper} ${styles[variants[variant] || 'alertModal']}`}>
-                    <h2 className={styles.title}>{title}</h2>
+                <div className={`${styles.wrapper} ${styles[variants[localConfig.variant] || 'alertModal']}`}>
+                    <h2 className={styles.title}>{localConfig.title}</h2>
 
-                    <p className={styles.text}>{text}</p>
+                    <p className={styles.text}>{localConfig.text}</p>
                     
-                    {variant == 'warning' ? (
+                    {localConfig.variant == 'warning' ? (
                         <div className={styles.buttons}>
-                            <Button variant="destructive" onClick={() => handleButtonClick(0)}>{buttonTexts[0] || "Yes"}</Button>
-                            <Button variant="secondary" onClick={() => handleButtonClick(1)}>{buttonTexts[1] || "Cancel"}</Button>
+                            <Button variant="destructive" onClick={() => handleButtonClick(0)}>{localConfig.buttonTexts[0] || "Yes"}</Button>
+                            <Button variant="secondary" onClick={() => handleButtonClick(1)}>{localConfig.buttonTexts[1] || "Cancel"}</Button>
                         </div>   
-                    ) : variant == 'alert' ? (
+                    ) : localConfig.variant == 'alert' ? (
                         <div  className={styles.buttons}>
-                            <Button variant="success" onClick={() => handleButtonClick(0)}>{buttonTexts[0] || "Okay" }</Button>
+                            <Button variant="success" onClick={() => handleButtonClick(0)}>{localConfig.buttonTexts[0] || "Okay" }</Button>
                         </div>
-                    ) : variant == 'input' ? (
+                    ) : localConfig.variant == 'input' ? (
                         <div className={styles.miniWrapper}>
-                            <input type="text" onChange={(e) => setModalInput(e.target.value)} value={modalInput}/>
+                            <input type="text" onChange={(e) => localConfig.setModalInput(e.target.value)} value={localConfig.modalInput}/>
                             <div>
-                                <Button variant="primary" onClick={() => handleButtonClick(0)}>{buttonTexts[0] || "Done"}</Button>
-                                <Button variant="secondary" onClick={() => handleButtonClick(1)}>{buttonTexts[1] || "Cancel"}</Button>
+                                <Button variant="primary" onClick={() => handleButtonClick(0)}>{localConfig.buttonTexts[0] || "Done"}</Button>
+                                <Button variant="secondary" onClick={() => handleButtonClick(1)}>{localConfig.buttonTexts[1] || "Cancel"}</Button>
                             </div>
                             
                         </div>
@@ -65,4 +77,4 @@ export default function Modal({
             
         </>
     )
-}
+})
