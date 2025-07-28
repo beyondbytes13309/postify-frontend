1f3a019a9ba95eb3ef26f83547505bead7968a0a
beyondbytes13309 <emmarthedev@gmail.com>
2025-07-28 18:46:42 +0500

    feat: add option menu component
    - used new component in comment section
    - used react portal in new component to avoid layout interference

--- src/components/comment/Comment.jsx
+++ src/components/comment/Comment.jsx
@@ -1,23 +1,47 @@
+import { useRef, useState } from 'react';
 import convertIsoToRelativeTime from '../../utils/isoToTimeAgo'
 import styles from '../styles/Comment.module.css'
 import { CiMenuKebab } from "react-icons/ci";
+import Options from '../common/Options';
 
 export default function Comment({ commentText, commentAuthor, profilePicURL, createdAt }) {
+    const [showOptions, setShowOptions] = useState(false)
+    const [position, setPosition] = useState({ x: 0, y: 0 })
+    const [selected, setSelected] = useState(null)
+
     return (
-        <>
-            <div className={styles.wrapper}>
-                <div className={styles.profile}>
-                    <img src={profilePicURL} className={styles.pfp} />
-                    <h2 className={styles.authorName}>{commentAuthor}</h2>
+      <>
+        <div className={styles.wrapper}>
+          <div className={styles.profile}>
+            <img src={profilePicURL} className={styles.pfp} />
+            <h2 className={styles.authorName}>{commentAuthor}</h2>
 
-                    <span className={styles.timeAgo}>{convertIsoToRelativeTime(createdAt)}</span>
-                    <button className={styles.commentMenuBtn}><CiMenuKebab className={styles.commentMenuIcon}/></button>
-                </div>
-                <div className={styles.body}>
-                    <p className={styles.commentText}>{commentText}</p>
-                </div>
-
-            </div>
-        </>
-    )
+            <span className={styles.timeAgo}>
+              {convertIsoToRelativeTime(createdAt)}
+            </span>
+            <button
+              onClick={(e) => {setPosition({x: e.clientX, y: e.clientY}); setShowOptions((prev) => !prev)}}
+              className={styles.commentMenuBtn}
+            >
+              <CiMenuKebab className={styles.commentMenuIcon} />
+            </button>
+            {showOptions && (
+              <Options
+                options={[
+                  { text: "have fun" },
+                  { text: "eat food" },
+                  { text: "ctrl+shift+del" },
+                ]}
+                position={position}
+                setSelected={setSelected}
+                setShowOptions={setShowOptions}
+              />
+            )}
+          </div>
+          <div className={styles.body}>
+            <p className={styles.commentText}>{commentText}</p>
+          </div>
+        </div>
+      </>
+    );
 }
---
+++ src/components/common/Options.jsx
@@ -0,0 +1,18 @@
+import styles from '../styles/Options.module.css'
+import { createPortal } from 'react-dom'
+
+export default function Options({ options=[], position, setSelected, setShowOptions }) {
+    const menu = (
+        <div className={styles.optionWrapper} style={{ top: position.y || 0, left: position.x || 0}}>
+            {options.map((option, index) => (
+                <button className={styles.option} key={index} onClick={() => {setSelected(index); setShowOptions(false)}}>{option.text || 'Default Text'}</button>
+            ))}
+        </div>
+    )
+
+    return (
+        <>
+            {createPortal(menu, document.body)}
+        </>
+    )
+}
