20bcfe3f48f63751aafcb7594ae30bea308d87c6
beyondbytes13309 <emmarthedev@gmail.com>
2025-07-29 16:18:10 +0500

    feat(options menu): use callback functions instead of indexes

--- src/components/common/Options.jsx
+++ src/components/common/Options.jsx
@@ -1,11 +1,11 @@
 import styles from '../styles/Options.module.css'
 import { createPortal } from 'react-dom'
 
-export default function Options({ options=[], position, setSelected, setShowOptions }) {
+export default function Options({ options=[], position, setShowOptions }) {
     const menu = (
         <div className={styles.optionWrapper} style={{ top: position.y || 0, left: position.x || 0}}>
             {options.map((option, index) => (
-                <button className={styles.option} key={index} onClick={() => {setSelected(index); setShowOptions(false)}}>{option.text || 'Default Text'}</button>
+                <button className={styles.option} key={index} onClick={() => {option.callback?.(); setShowOptions(false)}}>{option.text || 'Default Text'}</button>
             ))}
         </div>
     )
