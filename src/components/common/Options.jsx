da5e6f11b51ff58baa8e252f9afe59ff9d6fbfbc
beyondbytes13309 <emmarthedev@gmail.com>
2025-07-30 19:04:11 +0500

    fix: prevent error when option callback is not a function

--- src/components/common/Options.jsx
+++ src/components/common/Options.jsx
@@ -5,7 +5,7 @@ export default function Options({ options=[], position, setShowOptions }) {
     const menu = (
         <div className={styles.optionWrapper} style={{ top: position.y || 0, left: position.x || 0}}>
             {options.map((option, index) => (
-                <button className={styles.option} key={index} onClick={() => {option.callback?.(); setShowOptions(false)}}>{option.text || 'Default Text'}</button>
+                <button className={styles.option} key={index} onClick={() => {typeof option.callback == 'function' && option?.callback?.(); setShowOptions(false)}}>{option.text || 'Default Text'}</button>
             ))}
         </div>
     )
