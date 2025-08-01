aeb060a0fbddb779c9ef21aaee5eaefaff43df8f
beyondbytes13309 <emmarthedev@gmail.com>
2025-08-02 00:03:33 +0500

    feat: close options menu on outside click

--- src/components/common/Options.jsx
+++ src/components/common/Options.jsx
@@ -1,11 +1,27 @@
+import { useEffect, useRef } from "react";
 import styles from "../styles/Options.module.css";
 import { createPortal } from "react-dom";
 
 export default function Options({ options = [], position, setShowOptions }) {
+  const menuRef = useRef()
+
+  useEffect(() => {
+    const handleClick = (event) => {
+      if (!menuRef?.current?.contains(event.target)) {
+        setShowOptions(false)
+      } 
+    }
+
+    document.addEventListener('mousedown', handleClick)
+
+    return () => document.removeEventListener('mousedown', handleClick)
+  }, [])
+
   const menu = (
     <div
       className={styles.optionWrapper}
-      style={{ top: position.y || 0, left: position.x || 0 }}
+      style={{ top: position.y+4 || 0, left: position.x+4 || 0 }}
+      ref={menuRef}
     >
       {options.map((option, index) => (
         <button
